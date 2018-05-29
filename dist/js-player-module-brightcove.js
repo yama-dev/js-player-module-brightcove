0b4c53aba72985d9c286a91e261835064897b91c
yama-dev <tatsuya.yamamoto69@gmail.com>
2018-05-29 21:03:20 +0900

    admin: adjust version text.

--- dist/js-player-module-brightcove.js
+++ dist/js-player-module-brightcove.js
@@ -2,7 +2,7 @@
 /*!
  * JS PLAYER MODULE BRIGHTCOVE (JavaScript Library)
  *   js-player-module-brightcove.js
- * Version 2.0.8
+ * Version 2.0.9
  * Repository https://github.com/yama-dev/js-player-module-brightcove
  * Copyright yama-dev
  * Licensed under the MIT license.
