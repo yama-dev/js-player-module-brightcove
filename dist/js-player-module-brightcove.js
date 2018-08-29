e5f4b56b73b08082d33acf256d99ca8fbff5a69a
yama-dev <tatsuya.yamamoto69@gmail.com>
2018-08-29 10:40:49 +0900

    modify: adjust version text.

--- dist/js-player-module-brightcove.js
+++ dist/js-player-module-brightcove.js
@@ -2,7 +2,7 @@
 /*!
  * JS PLAYER MODULE BRIGHTCOVE (JavaScript Library)
  *   js-player-module-brightcove.js
- * Version 2.0.11
+ * Version 2.0.12
  * Repository https://github.com/yama-dev/js-player-module-brightcove
  * Copyright yama-dev
  * Licensed under the MIT license.
--- src/js-player-module-brightcove.js
+++ src/js-player-module-brightcove.js
@@ -1,7 +1,7 @@
 /*!
  * JS PLAYER MODULE BRIGHTCOVE (JavaScript Library)
  *   js-player-module-brightcove.js
- * Version 2.0.11
+ * Version 2.0.12
  * Repository https://github.com/yama-dev/js-player-module-brightcove
  * Copyright yama-dev
  * Licensed under the MIT license.
@@ -14,7 +14,7 @@ class PLAYER_MODULE_BRIGHTCOVE {
   constructor(options = {}){
 
     // Set Version.
-    this.VERSION = '2.0.11';
+    this.VERSION = '2.0.12';
 
     // Use for discrimination by URL.
     this.currentUrl = location.href;
