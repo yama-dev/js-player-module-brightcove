92fe18162903b35d5a94094167bf18c32699b70e
yama-dev <tatsuya.yamamoto69@gmail.com>
2018-12-03 20:05:33 +0900

    adjust versoin text.

--- src/js-player-module-brightcove.js
+++ src/js-player-module-brightcove.js
@@ -1,7 +1,7 @@
 /*!
  * JS PLAYER MODULE BRIGHTCOVE (JavaScript Library)
  *   js-player-module-brightcove.js
- * Version 2.0.12
+ * Version 2.1.0
  * Repository https://github.com/yama-dev/js-player-module-brightcove
  * Copyright yama-dev
  * Licensed under the MIT license.
