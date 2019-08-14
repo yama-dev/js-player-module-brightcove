168cd4772ca7394d001b641897433b1bc6ca3dee
yama-dev <tatsuya.yamamoto69@gmail.com>
2019-08-14 13:15:01 +0900

    update version text.

--- src/js-player-module-brightcove.js
+++ src/js-player-module-brightcove.js
@@ -1,7 +1,7 @@
 /*!
  * JS PLAYER MODULE BRIGHTCOVE (JavaScript Library)
  *   js-player-module-brightcove.js
- * Version 3.2.0
+ * Version 4.0.0
  * Repository https://github.com/yama-dev/js-player-module-brightcove
  * Copyright yama-dev
  * Licensed under the MIT license.
@@ -19,7 +19,7 @@ export default class PLAYER_MODULE_BRIGHTCOVE {
   constructor(options = {}){
 
     // Set Version.
-    this.VERSION = '3.2.0';
+    this.VERSION = '4.0.0';
 
     // Set Flgs.
     this.PlayerChangeLoadFlg = true;
