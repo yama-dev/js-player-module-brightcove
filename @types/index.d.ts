2eedda3b251cf0625ff42bd8004264f9b00e1786
yama-dev <tatsuya.yamamoto69@gmail.com>
2021-10-09 19:50:22 +0900

    adjust type

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,93 +1,9 @@
 
-declare function test(): string;
+declare module '@yama-dev/js-dom';
 
-declare module '@yama-dev/js-dom/core';
-declare module '@yama-dev/js-parse-module/libs';
+declare module '@yama-dev/js-parse-module/dist/js-parse-module';
 
-interface MyInterface{
-  name: string
-  init(): any 
-}
-
-interface PlayerModuleBrightcoveInterface {
-  VERSION: any
-  PlayerChangeLoadFlg: boolean
-  CONFIG: {
-    mode: string
-    id  : string
-
-    player_id: string
-    player_id_wrap: string
-    player_ui_id: string
-    player_style_id: string
-
-    videoid: string | number
-    account: string | number
-    width: string | number
-    height: string | number
-
-    player: string | number
-    volume: number
-
-    playsinline: string
-    loop: string
-    muted: string // '' | 'muted'
-
-    ui_controls: string
-    ui_autoplay: string
-    ui_default: boolean
-    ui_default_css: boolean
-
-    stop_outfocus: boolean
-    poster: boolean
-
-    add_style: string
-    classname_active_wrap: string
-    classname_active: string
-  }
-  PlayerMediaInfo: any
-  Player: any
-  $: {
-    playerElem                 : any
-    playerElemMainWrap         : any
-    uiBtnPlay                  : any
-    uiBtnStop                  : any
-    uiBtnPause                 : any
-    uiBtnMute                  : any
-    uiBtnVolon                 : any
-    uiBtnVoloff                : any
-    uiDisplayTime              : any
-    uiDisplayTimeNow           : any
-    uiDisplayTimeTotal         : any
-    uiDisplayTimeDown          : any
-    uiDisplayTimePar           : any
-    uiDisplayPoster            : any
-    uiDisplayPosterBg          : any
-    uiDisplayName              : any
-    uiSeekbarVol               : any
-    uiSeekbarVolBg             : any
-    uiSeekbarVolCover          : any
-    uiSeekbarTime              : any
-    uiSeekbarTimeBg            : any
-    uiSeekbarTimeCover         : any
-    uiBtnChange                : any
-    uiBtnDataId                : any
-  }
-  playerHtml: any
-  playerCss: any
-  playerScriptCode: any
-  playerUiHtml: string
-  playerCssOption: string
-  on: any
-  PlayerJson?: any
-  PlayerChangeSeekingFlg: boolean
-
-  state: {
-    poster: string
-  }
-}
-
-declare function videojs(id: string): void
+declare function videojs(id: string): void;
 
 interface Window {
   PLAYER_MODULE_ALL_PLATLIST: any
